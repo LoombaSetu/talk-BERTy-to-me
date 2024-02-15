58b2e31e88582be90b9c54d17ce3c883e4f23839
Setu Loomba <LoombaSetu>
2024-02-15 11:45:35 -0600

    Fixed dataloader

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -7,28 +7,31 @@ os.chdir("D:/University/Projects/AML/talk-berty-to-me") #Change path to root dir
 
 class TextCorpus():
     __slots__ = ['path', 'vocab', 'data', 'flag']
-    def __init__(self, *args, vocab_exists = False):
-        if 'train' in args:
+    def __init__(self, arg, vocab_exists = False):
+        if arg == 'train':
             self.flag = 'train'
             self.path = 'data/train.parquet'
-        elif 'test' in args:
+        elif arg == 'test':
             self.flag = 'test'
             self.path = 'data/test.parquet'
-        elif 'val' in args:
+        elif arg == 'val':
             self.flag = 'val'
             self.path = 'data/val.parquet'
-        elif 'dev' in args:
+        elif arg == 'dev':
             self.flag = 'dev'
             self.path = 'data/dev.parquet'
         self.data = pd.read_parquet(self.path)
         if not vocab_exists:
             self.vocab = self.build_vocab()
         else:
-            self.vocab = torch.load('vocab' + self.flag + '.pt')
+            # self.vocab = torch.load('vocab' + self.flag + '.pth')
+            self.vocab = torch.load('data/vocab.pth')
+            self.vocab.set_default_index(self.vocab["<unk>"])
 
     def yield_tokens(self):
         tokenizer = get_tokenizer("basic_english")
-        train_iter = iter(self.data.loc[:,'text'] + self.data.loc[:,'title'] + self.data.loc[:,'genre_one_hot'])
+        train_iter = iter(self.data.loc[:,'text'] + self.data.loc[:,'title']
+                           + self.data.loc[:,'genre_one_hot'])
         for text in train_iter:
             if type(text) is not str:
                 continue
@@ -38,5 +41,5 @@ class TextCorpus():
         vocab = build_vocab_from_iterator(self.yield_tokens(),
                                            specials=["<unk>"], min_freq= 1000)
         vocab.set_default_index(vocab["<unk>"])
-        torch.save(vocab, 'vocab' + self.flag + '.pt')
+        torch.save(vocab, 'vocab' + self.flag + '.pth')
         return vocab
--- scripts/pre-process.py
+++ scripts/pre-process.py
@@ -1,41 +1,57 @@
-from load_data import TextCorpus
-import pandas as pd
-#import os
 import torch
+import nltk
 from torch.nn.utils.rnn import pad_sequence
 from torchtext.data.utils import get_tokenizer
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
+from load_data import TextCorpus
+nltk.download("punkt")
+nltk.download("stopwords")
 
-#os.chdir("D:/University/Projects/AML/talk-berty-to-me") #Change path to root directory of project
-
-
-def create_seq2seq_labels(text, window_size = 8):
+def create_seq2seq_labels(text, window_size=8):
+    if text is None:
+        return []
     tokens = tokenizer(text)
     sequence_pairs = []
-    for i in range(len(tokens) - window_size):
-        sequence_pairs.append((tokens[i:i+window_size], tokens[i+1:i+window_size+1]))
+    for idx in range(len(tokens) - window_size):
+        sequence_pairs.append(
+            (tokens[idx : idx + window_size], tokens[idx + 1 : idx + window_size + 1])
+        )
     return sequence_pairs
 
-
-def collate_batch(batch, vocab):
+def collate_batch(batch):
     titles, genres, seq_pair = zip(*batch)
+    titles = [tokenizer(t) for t in titles]
+    title_tensor = pad_sequence([torch.tensor(vocab.lookup_indices(t)) for t in titles],
+                                 batch_first=True)
+    genres = [tokenizer(str(t)) for t in genres]
+    genre_tensor = pad_sequence([torch.tensor(vocab.lookup_indices(t)) for t in genres],
+                                batch_first=True)
     input_seq, output_seq = zip(*seq_pair)
-    title_tensor = pad_sequence([torch.tensor(vocab.lookup_indices(tokenizer(t))) for t in titles],
-                            batch_first=True)
-    input_tensor = pad_sequence([torch.tensor(vocab.lookup_indices(t)) for t in input_seq],
-                            batch_first=True)
-    output_tensor = pad_sequence([torch.tensor(vocab.lookup_indices(t)) for t in output_seq],
-                            batch_first=True)
-    cat_tensor = pad_sequence([torch.tensor(vocab.lookup_indices(tokenizer(t))) for t in genres],
-                            batch_first=True)
-    return title_tensor, input_tensor, output_tensor, cat_tensor
+    input_tensor = pad_sequence(
+        [torch.tensor(vocab.lookup_indices(t)) for t in input_seq], batch_first=True
+    )
+    output_tensor = pad_sequence(
+        [torch.tensor(vocab.lookup_indices(t)) for t in output_seq], batch_first=True
+    )
+    return title_tensor, input_tensor, output_tensor, genre_tensor
 
-corpus = TextCorpus('dev', True)
+
+corpus = TextCorpus("dev", True)
 tokenizer = get_tokenizer("basic_english")
 train_df = corpus.data
-train_df.loc[:,'seq_pairs'] = train_df['text'].apply(create_seq2seq_labels)
-train_df.drop(columns=['text'], inplace=True)
-train_df = train_df.explode('seq_pairs').reset_index(drop=True)
+train_df.loc[:, "seq_pairs"] = train_df["text"].apply(create_seq2seq_labels)
+train_df.drop(columns=["text"], inplace=True)
+train_df = train_df.explode("seq_pairs").reset_index(drop=True)
+print(train_df.columns)
+print(train_df.loc[:, "seq_pairs"][:1])
 vocab = corpus.vocab
+
+dataloader = torch.utils.data.DataLoader(
+    train_df.values, batch_size=8, shuffle=False, collate_fn=collate_batch
+)
+for i, (title, input_seq, output_seq, genre) in enumerate(dataloader):
+    print(title)
+    print(input_seq)
+    print(output_seq)
+    print(genre)
+    if i == 0:
+        break
